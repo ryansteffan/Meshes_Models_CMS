5af418d65192b8eb6ac791e9ba81966c63b90b6d
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-11-03 15:58:46 -0600

    Created the initial structure for the project.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require("utilities/connect.php");
 
 ?>
 
--- pages/db_error.php
+++ pages/db_error.php
@@ -1,17 +1,15 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Oh No!</title>
 </head>
 
 <body>
+
+    <h1></h1>
 
 </body>
 
