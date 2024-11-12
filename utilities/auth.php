4cc2b4d65e180f28b773851609f6e3e13b630320
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-11-12 15:19:25 -0600

    Fixed a PDO Binding due to salt being removed. Auth is now complete.

--- utilities/auth.php
+++ utilities/auth.php
@@ -26,26 +26,19 @@ function set_user_logged_out()
 
 function create_user($database, $email, $password, $first_name, $last_name, $auth_level)
 {
-    // Make a 16 bit salt value that prevents rainbow table lookup.
-    $password_salt = bin2hex(random_bytes(16));
-
-    $password = $password;
-
-    // Combine the salt with the hash.
-    $combined_hash_and_salt = password_hash($password, PASSWORD_BCRYPT);
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
     $query = "INSERT INTO Users (
-                email, hash, salt, first_name, last_name, auth_level
+                email, hash, first_name, last_name, auth_level
               )
               VALUES (
-                :email, :combined_hash, :salt, :fn, :ln, :auth_level
+                :email, :combined_hash, :fn, :ln, :auth_level
                 );";
 
     $statement = $database->prepare($query);
 
     $statement->bindValue(":email", $email);
-    $statement->bindValue(":combined_hash", $combined_hash_and_salt);
-    $statement->bindValue(":salt", $password_salt);
+    $statement->bindValue(":combined_hash", $hashed_password);
     $statement->bindValue(":fn", $first_name);
     $statement->bindValue(":ln", $last_name);
     $statement->bindValue(":auth_level", $auth_level, PDO::PARAM_INT);
@@ -55,21 +48,23 @@ function create_user($database, $email, $password, $first_name, $last_name, $aut
 
 function login_user($database, $username, $password)
 {
-    $user_query = "SELECT email, hash, salt FROM users WHERE email = :username;";
+    $user_query = "SELECT email, hash FROM users WHERE email = :username;";
     $statement = $database->prepare($user_query);
     $statement->bindValue(":username", $username);
     $statement->execute();
 
     $user_row = $statement->fetch();
 
-    $stored_password_hash = $user_row["hash"];
-    $stored_password_salt = $user_row["salt"];
-
-    $password = $password;
+    try {
+        $stored_password_hash = $user_row["hash"];
+    } catch (Exception $e) {
+        header("Location: ../pages/login.php");
+    }
 
     $isValidLogin = password_verify($password, $stored_password_hash);
     if ($isValidLogin) {
         set_user_logged_in();
+        header("Location: ../index.php");
     } else {
         header("../pages/login.php");
     }
