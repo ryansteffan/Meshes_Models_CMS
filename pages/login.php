29783f004be006bd8d7be5d6e0c8cced5fd35eef
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-11-12 12:14:43 -0600

    Added create_user function to the auth.php library. Added basic login page html

--- pages/login.php
+++ pages/login.php
@@ -8,10 +8,19 @@ require("../utilities/connect.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 
 <body>
+
+    <div id="login_island">
+        <form action="#" method="post">
+            <label for="email">Username/Email:</label>
+            <input type="text" name="email" id="email">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </form>
+    </div>
 
 </body>
 
--- utilities/auth.php
+++ utilities/auth.php
@@ -1,4 +1,7 @@
 <?php
+// Encryption info: 
+// https://medium.com/@mrityunjay.webmaster/how-to-secure-hash-and-salt-for-php-passwords-54f1c9d268a6
+
 require("connect.php");
 
 if (session_id() == '') {
@@ -20,10 +23,38 @@ function set_user_logged_out()
     $_SESSION["logged_in"] = false;
 }
 
+function create_user($database, $email, $password, $first_name, $last_name, $auth_level)
+{
+    // Hash the password
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    // Make a 16 bit salt value that prevents rainbow table lookup.
+    $password_salt = bin2hex(random_bytes(16));
+
+    // Combine the salt with the hash.
+    $combined_hash_and_salt = password_hash($password_hash . $password_salt, PASSWORD_BCRYPT);
+
+    $query = "INSERT INTO Users (
+                email, hash, salt, first_name, last_name, auth_level
+              )
+              VALUES (
+                ':email', ':combined_hash', ':salt', ':fn', ':ln', :auth_level
+                );";
+
+    $database->prepare($query);
+
+    $database->bindValue(":email", $email);
+    $database->bindValue(":combined_hash", $combined_hash_and_salt);
+    $database->bindValue(":salt", $password_salt);
+    $database->bindValue(":fn", $first_name);
+    $database->bindValue(":ln", $last_name);
+    $database->bindValue(":auth_level", $auth_level, PDO::PARAM_INT);
+
+    $database->execute();
+}
+
 function login_user($database, $username, $password, $required_auth_level)
 {
-    $password_hash = $password;
-
     $user_query = "SELECT email, password, auth_level FROM users WHERE email = :username;";
     $database->prepare($user_query);
     $database->bindValue(":username", $username);
@@ -31,5 +62,12 @@ function login_user($database, $username, $password, $required_auth_level)
 
     $user_row = $database->fetch();
 
-    $email = $user_row["email"];
+    $stored_email = $user_row["email"];
+    $stored_password_hash = $user_row["hash"];
+    $stored_password_salt = $user_row["salt"];
+    $stored_auth_level = $user_row["auth_level"];
+
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    $password_salt = bin2hex(random_bytes(16));
+    $combined_salt_and_hash = password_hash($password_hash . $password_salt, PASSWORD_BCRYPT);
 }
