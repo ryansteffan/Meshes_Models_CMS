87b8120f9a7e118025c1c5982997d2c69d510dcc
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-12-09 20:28:36 -0600

    Fixed bug when date of posts in the categories view.

--- utilities/categories.php
+++ utilities/categories.php
@@ -32,7 +32,7 @@ function get_full_category_data($db)
 // Gets all of the posts in a given category.
 function get_category_posts($db, $categories)
 {
-    $query = "SELECT p.post_id, first_name, last_name, written_content, image_content, post_date 
+    $query = "SELECT p.post_id, first_name, last_name, written_content, image_content, post_date, modified_date
               FROM posts p
               JOIN users u
               ON p.author = u.user_id
